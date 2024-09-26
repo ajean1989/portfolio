2df2114d6300677a1cf28774500d33df9dba0659
ajean1989 <105272808+ajean1989@users.noreply.github.com>
2024-09-26 11:29:03 +0200

    debug azure deployment

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import './assets/main.css'
-import './index.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import './assets/main.css'
+import './index.css'
 
 import App from './App.vue'
 import router from './router'
